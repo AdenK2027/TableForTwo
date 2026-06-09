764ad744742e71d6b4e1c06483878cd53b6ad10e
Aden Kieffaber <aden.kieffaber.23@cnu.edu>
2026-06-09 13:14:59 -0400

    implemented dynamic grocery list display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [groceryListOut, setGroceryListOut] = useState(false);
+
   return (
-    <>
-    <h1>Hello</h1>
-    <img src="/assets/GroceryList.png" width="500vw"></img>
-    </>
+    <div className="mainFrame">
+      <h1>Table For Two</h1>
+      <div className={`groceryListContainer ${groceryListOut ? 'open' : ''}`}
+      onClick={() => {
+        setGroceryListOut(!groceryListOut);
+      }}>
+        <img src="/assets/GroceryList.png" className="groceryList"></img>
+      </div>
+    </div>
   )
 }
 
